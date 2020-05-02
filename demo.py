7aa68d2cc449a85baa94fa0127bd96faa8622a87
CatarinaJunqueira <catarinajunqueira@hotmail.com>
2020-05-02 18:50:55 -0300

    commit teste

--- demo.py
+++ demo.py
@@ -1,2 +1,22 @@
-print("Hello")
-print("Teste")
+from scipy.io import loadmat
+import numpy as np
+import CreateData as CreateData
+
+def RegraAleatoria(casename):
+
+    data = loadmat(casename)
+    Patio, Porto, R, C = CreateData(data)
+    print('parametros criados')
+
+
+
+
+if __name__ == "__main__":
+
+
+    X = [1, 2 , 3, 5, 6, 4, 9, 10, 11, 12, 13, 14]
+    #X = [7]
+    for i in X:
+        name_instance1 = 'InstanciaModeloIntegrado_' + str(i) + '.mat'
+        print(name_instance1)
+        _ = RegraAleatoria(name_instance1)
